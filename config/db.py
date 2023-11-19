4261e00abe54f083a7b81c2a5add9d453801e673
Daniel Mercado <dms9812@hotmail.com>
2023-11-19 14:18:50 -0500

    db credentials

--- config/db.py
+++ config/db.py
@@ -1,13 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from config.db_credentials import db_config
 
 app = Flask(__name__)
 
-db_user = 'your_user'
-db_pass = 'your_pass'
-db_host = 'your_host'
-db_name = 'your_dbname'
+db_user = db_config['db_user']
+db_pass = db_config['db_pass']
+db_host = db_config['db_host']
+db_name = db_config['db_name']
 
 app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{db_user}:{db_pass}@{db_host}/{db_name}"
 app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
